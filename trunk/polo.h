77234208b1158a1fcab3f2f29b6c5cd61d51ff50
mressl <mressl@7da9b1ee-236c-63a8-5953-22710ba1ada3>
2011-02-10 01:03:39 +0000

    Structure update

--- trunk/polo.h
+++ trunk/polo.h
@@ -14,13 +14,15 @@
  * - Add polo.c and polo.h to your project.
  * - polo continuously calls a user function to redraw the screen.
  *   Call setDrawCallback() to define this function.
- * - Call runGraphics() to run graphics.
+ * - Call runGraphics() to run graphics and get your user function called.
  * - To exit graphics, call exitGraphics() from your user function.
- * - You must change settings and draw from the user function.
+ * - Always draw from your user function.
  *
  * Specifications:
  * - The coordinate system (0, 0) is at the lower left.
- * - Color components (RGB, HSV, HSB) are in the range 0..255
+ * - Color components (RGB, HSV, HSB) are in the range 0..255.
+ * - Images must be in uncompressed BMP format, using either
+ *   24-bit RGB or 32-bit RGBA (for transparency).
  *
  */
 
@@ -29,15 +31,34 @@
 
 // Initialization
 void setDrawCallback(void *drawCallback(void));
-void runGraphics(int width, int height, int fullscreen);
+void runGraphics(int width, int height, int fullscreen, char *windowTitle);
+void exitGraphics();
 
 // Colors
-Color getColorFromRGB(int red, int green, int blue);
-Color getColorFromHSV(int hue, int saturation, int value);
-Color getColorFromHSB(int hue, int saturation, int brightness);
+enum
+{
+	POLO_TRANSPARENT	= 0x00000000,
+	POLO_RED 		= 0xff0000ff,
+};
+
+isMouseButtonPressed()
+
+Color getColorFromRGBA(unsigned char red,
+                       unsigned char green,
+                       unsigned char blue,
+                       unsigned char alpha);
+Color getColorFromHSVA(unsigned char hue,
+                       unsigned char saturation,
+                       unsigned char value,
+                       unsigned char alpha);
+Color getColorFromHSBA(unsigned char hue,
+                       unsigned char saturation,
+                       unsigned char brightness,
+                       unsigned char alpha);
 
 void setPenColor(Color color);
-void setFillColor(Color color1, Color color2);
+void setFillColor(Color color);
+void setGradientFillColors(Color color1, Color color2);
 
 // Drawing Primitives
 void drawPoint(float x, float y);
@@ -49,7 +70,8 @@ void drawQuad(float x1, float y1, float x2, float y2, float x3, float y3, float 
 void drawCircle(float x, float y, float radius);
 
 // Text
-enum PoloFont {
+enum PoloFont
+{
 	POLO_COURIER_8,
 	POLO_COURIER_9,
 	POLO_TIMES_10,
@@ -60,7 +82,7 @@ enum PoloFont {
 };
 void setTextFont(PoloFont font);
 int getTextDrawWidth(char *str);
-drawText(float x, float y, char *str);
+void drawText(float x, float y, char *str);
 
 // Images
 Image loadImage(char *path);
@@ -73,5 +95,11 @@ float getMouseY();
 float isMouseButtonPressed(int buttonIndex);
 
 // Keyboard
-int getKeyboardKey();
+int getPressedKey();
+
+// Advanced functions
+void setFrameRate(float framesPerSecond);
+int getFrameCount();
+void showMousePointer();
+void hideMousePointer();
 
