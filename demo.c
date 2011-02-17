f780713e8663fa3e6af5f749a0b2c560cf17881e
jorge.prendes <jorge.prendes@gmail.com>
2011-02-17 20:36:58 +0000

    Now compiles on cpp

--- demo.c
+++ demo.c
@@ -10,43 +10,28 @@ void draw(void *userData)
 {
 	DemoData *d = (DemoData *) userData;
 	
-	static int t = 0;
-	
 	if (getKey() == 27)
 		exitPolo();
 	
-/*	setPenColor(POLO_TRANSPARENT);
-	setGradientFillColors(POLO_IRON, POLO_WHITE);
-	drawRect(0, 0, 80, 480);
-*/	
 	if (isMouseButtonPressed(1))
 		clearScreen();
 	if (isMouseButtonPressed(0))
 		drawImage(getMouseX() - getImageWidth(d->image) / 2,
 		          getMouseY() - getImageHeight(d->image) / 2,
 		          d->image);
-	
-	{
-//	setGradientFillColors(POLO_CANTALOUPE, POLO_MARASCHINO);
-	setPenColor(POLO_TRANSPARENT);
-	setFillColor((t % 2) ? POLO_WHITE : POLO_BLACK);
-	drawRect(0, 0, 640, 480);
-	}
-	
-	t++;
 }
 
 int main(int argc, char *argv[])
 {
 	DemoData demoData;
 	
-	initPolo(640, 480, 1, "Mouse Painter");
+	initPolo(640, 480, 0, "Mouse Painter");
 	setPoloUserData(&demoData);
 	
 	setDrawCallback(draw);
-	setImageAlpha(0.1);
 	
-	demoData.image = loadImage("demo.bmp");
+	setImageAlpha(0.1);	
+	demoData.image = loadImage("brush.bmp");
 	
 	runPolo();
 }
