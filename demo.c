e43a185237a39eb25a04cdc0fe9f5477e8a9c693
mressl <mressl@7da9b1ee-236c-63a8-5953-22710ba1ada3>
2011-02-18 00:51:15 +0000

    Added rounded rect

--- demo.c
+++ demo.c
@@ -13,18 +13,27 @@ void draw(void *userData)
 	
 	if (getKey() == 27)
 		exitPolo();
+		
+	setPenColor(POLO_TRANSPARENT);
+	setFillColor(0x00000008);
+	drawRect(0, 0, 640, 480);
 	
-/*	if (isMouseButtonPressed(1))
+	if (isMouseButtonPressed(1))
 		clearScreen();
 	if (isMouseButtonPressed(0))
+	{
+//		setPenColor(POLO_TRANSPARENT);
+//		setGradientFillColors(0x00000040, 0x00000000);
+//		drawCircle(getMouseX(), getMouseY(), 512);
 		drawImage(getMouseX() - getImageWidth(d->image) / 2,
 		          getMouseY() - getImageHeight(d->image) / 2,
 		          d->image);
-*/	
+	}
+	
 	setPenColor(POLO_TUNGSTEN);
 	setGradientFillColors(POLO_SILVER, POLO_TUNGSTEN);
 //	drawRect(-1, -1, 128, getScreenHeight() + 2);
-	drawRoundedRect(0, 0, getScreenWidth(), getScreenHeight(), 4000);
+	drawRoundedRect(0, 0, 128, getScreenHeight(), 16);
 	
 	setPenColor(POLO_BLACK);
 	drawText( (128-getTextDrawWidth(text))/2,
@@ -41,7 +50,7 @@ int main(int argc, char *argv[])
 	setPoloUserData(&demoData);
 	setDrawCallback(draw);
 	setImageAlpha(0.1);
-	demoData.image = loadImage("brush.bmp");
+	demoData.image = loadImage("demo.bmp");
 	
 	runPolo();
 }
