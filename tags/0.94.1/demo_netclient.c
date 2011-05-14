ee3baf9a418c1ee6a331ec8ec1569501280c02cd
mressl@gmail.com <mressl@gmail.com@7da9b1ee-236c-63a8-5953-22710ba1ada3>
2011-05-14 07:12:24 +0000

    Added 0.94.1

---
+++ tags/0.94.1/demo_netclient.c
@@ -0,0 +1,91 @@
+
+/**
+ * libpolonet client demo
+ * Simple client demo
+ * (C) 2011 by the libpolo team.
+ *     Marc S. Ressl (mressl@itba.edu.ar)
+ *     Jorge Prendes (jprendes@itba.edu.ar)
+ * Released under the GPL
+ */
+
+/*
+ * Description:
+ *
+ * This demo attempts to download the main page of http://www.google.com
+ */
+
+#include <stdio.h>
+#include <string.h>
+
+#include "polonet.h"
+
+#define HOSTNAME "www.google.com"
+
+char getCommand[] =
+"GET / HTTP/1.1\n"
+"Host:" HOSTNAME "\n"
+"\n";
+
+/* Convenience method to receive data */
+int getData(PolonetConn conn, char *buffer, int buffersize)
+{
+	int bytesReceived;
+	
+	while (isConnected(conn))
+	{
+		if (bytesReceived = receiveData(conn, buffer, buffersize))
+			return bytesReceived;
+		
+		/* Wait 10 milliseconds, so the CPU is not clogged */
+		usleep(10000);
+	}
+	
+	/* Just in case there is data left */ 
+	if (bytesReceived = receiveData(conn, buffer, buffersize))
+		return bytesReceived;
+	
+	return 0;
+}
+
+int main(int argc, char *argv[])
+{
+	PolonetConn conn;
+	char buffer[256];
+	int bytesReceived;
+	
+	/* Open the connection */
+	if (!(conn = openConnection(HOSTNAME, 80)))
+	{
+		printf("Couldn't connect to " HOSTNAME ".\n");
+		return 1;
+	}
+	
+	/* Wait for the connection establishment.
+	   If the connection is pending, wait 10 milliseconds */
+	while (isPending(conn))
+		usleep(10000);
+	
+	if (!isConnected(conn))
+	{
+		/* Always close open connections */
+		closeConnection(conn);
+		
+		printf("Connection refused from " HOSTNAME ".\n");
+		return 1;
+	}
+
+	/* Send the GET command */
+	sendData(conn, getCommand, strlen(getCommand));
+	
+	/* Receive the response */
+	while (bytesReceived = getData(conn, buffer, sizeof(buffer) - 1))
+	{
+		buffer[bytesReceived] = '\0';
+		printf("%s", buffer);
+	}
+	
+	closeConnection(conn);
+	
+	return 0;
+}
+
---
+++ tags/0.94.1/polonet.h
@@ -0,0 +1,68 @@
+/**
+ * libpolonet
+ * Lightweight TCP/IP network module for educational environments
+ * (C) 2011 by the libpolo team.
+ *     Marc S. Ressl (mressl@itba.edu.ar)
+ *     Jorge Prendes (jprendes@itba.edu.ar)
+ *     Agustin Perez Moreno
+ * Released under the GPL
+ */
+
+/*
+ * Polonet is capable of making and receiving TCP/IP connections.
+ *
+ * Usage as a client:
+ * - Call openConnection() to connect to a host. This returns a
+ *   PolonetConn identifier. If the identifier is 0, the connection
+ *   could not be established
+ *
+ * Usage as a server:
+ * - Call startListening() to start listening to connections on a port.
+ * - getAvailableConnection() gets the next available incoming connection.
+ *   If there is no incoming connection, it returns 0.
+ * - You can use the client functions to handle the incoming connection.
+ *
+ * Managing a connection:
+ * - isPending() determines if a connection is being established.
+ * - isConnected() determines if a connection is established.
+ * - Call sendData() to send bytes to a connection. It returns
+ *   the number of bytes sent.
+ * - Call receiveData() to receive bytes from a connection. It returns
+ *   the number of bytes received.
+ * - Call closeConnection() to close a connection. You should always
+ *   close connections you received from openConnection or
+ *   getAvailableConnection().
+ * - Note: all calls are non-blocking.
+ * - Note: there might be data left for reading, even though a
+ *   connection is closed.
+ */
+
+#ifndef _LIBPOLONET_H
+#define _LIBPOLONET_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef int PolonetConn;
+
+/* Client functions */
+PolonetConn openConnection(char *hostname, unsigned short port);
+
+/* Server functions */
+int startListening(unsigned short port);
+void stopListening();
+PolonetConn getAvailableConnection();
+
+/* Connection functions */
+int isPending(PolonetConn conn);
+int isConnected(PolonetConn conn);
+int sendData(PolonetConn conn, char *buffer, const int bufferSize);
+int receiveData(PolonetConn conn, char *buffer, const int bufferSize);
+void closeConnection(PolonetConn conn);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
