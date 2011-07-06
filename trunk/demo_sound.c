dfb902ebf9f3a30f0fc1af9507836ef96454995e
mressl <mressl@7da9b1ee-236c-63a8-5953-22710ba1ada3>
2011-07-06 19:41:05 +0000

    Added polosnd, fixed polonet receiveData() when running a server connection (was blocking), fixed BMP width alignment when loading images

---
+++ trunk/demo_sound.c
@@ -0,0 +1,36 @@
+/**
+ * libpolosnd demo
+ * Sound demo program
+ * (C) 2011 by the libpolo team.
+ *     Marc S. Ressl (mressl@itba.edu.ar)
+ * Released under the GPL
+ *
+ * Requires libpolosnd.
+ */
+
+#include <stdio.h>
+#include "polosnd.h"
+
+int main(int argc, char *argv[])
+{
+	/* Open the sound device */
+	openSound();
+	
+	/* Play back a sound */
+	playSound("demo_sound.ogg");
+	usleep(200000);
+	
+	/* Wait a second */
+	stopSound();
+	usleep(500000);
+	
+	/* Play again */
+	playSound("demo_sound.ogg");
+	sleep(3);
+	
+	/* Close the sound device */
+	closeSound();
+	
+	return 0;
+}
+
